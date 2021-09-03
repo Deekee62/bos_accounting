8cf6ff2353b196037d4f57df4366c50f259897cf
BhaagBodeDK <vaibhav.cbrain@gmail.com>
2021-09-03 17:23:02 +0000

    Remove bc dependency

--- bos_accounting.sh
+++ bos_accounting.sh
@@ -7,11 +7,10 @@
 # The results are written to  stdout which can be redirected to a log file by cron.
 #
 # BOS (Balance of Satoshi) needs to be installed
-# bc needs to be installed (sudo apt-get bc)
 #
 # Add the following in crontab to run regulary. Change path as appropriate
-# 55 23 * * * ~/bos_accounting/bos_accounting.sh >> ~/routed.txt 2>&1
-# Version: 0.0.5
+# 55 23 * * * ~/bos_accounting/bos_accounting.sh >> ~/bos_accounting.log 2>&1
+# Version: 0.0.7
 # Author: Dirk Krienbuehl https://t.me/Deekee62
 # Additions : VS https://t.me/BhaagBoseDk : Removing lncli and icreasing compatibilities with other installations.
 #
@@ -24,7 +23,7 @@ BOS=`which bos`
 if [ "$BOS" == "" ] || [ ! -f $BOS ]
 then
 	# Potential Docker Installation
-	BOS="docker run -it --rm --network=host --add-host=umbrel.local:10.21.21.9 -v $HOME/.bos:/home/node/.bos -v $HOME/umbrel/lnd:/home/node/.lnd:ro alexbosworth/balanceofsatoshis"
+	BOS="docker run --rm --network=host --add-host=umbrel.local:10.21.21.9 -v $HOME/.bos:/home/node/.bos -v $HOME/umbrel/lnd:/home/node/.lnd:ro alexbosworth/balanceofsatoshis"
 fi
 #BOS=user_specific_path for bos
 
@@ -45,22 +44,22 @@ d_paid="$($BOS chart-fees-paid  --days 7 | /bin/grep 'Total:' | /usr/bin/awk '{p
 e_chainpaid="$($BOS chart-chain-fees  --days 7 | grep 'Total:' | awk '{print $10}' | sed -r -e 's/[[:cntrl:]]\[[0-9]{1,3}m//g' -e 's/\n/ /g' -e 's/^0.//' |  tr -d '\r')"
 #
 # Calculate the percentage of the forwared sats compared to the local channel balance for the last 7 days
-f_pcrouted=$(echo "scale=2; 100/($a_local/$b_routed)" | /usr/bin/bc -l)
+f_pcrouted=`printf "%0.2f" $((100/(10#$a_local/10#$b_routed)))`
 #
 # Calculate the ppm of the fees earned compared to the local channel balance for the last 7 days
-g_ppmearned=$(echo "scale=0; 1000000/($a_local/$c_earned)" | bc -l)
+g_ppmearned=$((1000000/(10#$a_local/10#$c_earned)))
 #
 # Calculate the ppm of the fees paid compared to the local channel balance for the last 7 days
 #
-h_ppmpaid=$(echo "scale=0; 1000000/($a_local/$d_paid)" | bc -l)
+h_ppmpaid=$((1000000/(10#$a_local/10#$d_paid)))
 #
 # Calculate the ppm of the net fees paid compared to the local channel balance for the last 7 days
 #
-i_ppmnet=$(echo "scale=0; 1000000/($a_local/($c_earned-$d_paid-$e_chainpaid))" | bc -l)
+i_ppmnet=$((1000000/(10#$a_local/(10#$c_earned-10#$d_paid-10#$e_chainpaid))))
 #
 # Calculate the sats of net fees earned
 #
-k_netearned=`printf "%08d" $(echo "scale=0; ($c_earned-$d_paid-$e_chainpaid)" | bc -l)`
+k_netearned=`printf "%08d" $((10#$c_earned-10#$d_paid-10#$e_chainpaid))`
 #
 # Print year, time, local channel balance, forwarded amount, % forwarded, fees earned ppm, fees paid ppm, fees net ppm, amount fees earned, amount fees paid, amount chain fees, amount fees net
 #
